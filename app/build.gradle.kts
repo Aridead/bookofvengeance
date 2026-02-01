3b7010bb903d0ca8fbdf2b98d44e5e59c5e7c238
Aridead <josephmengele1161@gmail.com>
2026-02-01 23:48:19 +0300

    Fix theme parent and add material dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.foundation:foundation")
     implementation("androidx.compose.material:material-icons-extended")
+    implementation("com.google.android.material:material:1.12.0")
 
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
